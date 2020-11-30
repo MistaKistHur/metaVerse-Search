a8f80fe61378e95ff46a6bba72757f0f39013ea5
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-11-30 07:34:27 +0000

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,1 +1,17 @@
+## create script to crawl sites
+## https://dev.to/fprime/how-to-create-a-web-crawler-from-scratch-in-python-2p46
+## beautyful soup, http, https, cors, search for SO questions related
+import requests    
+import re    
 
+class PyCrawler(object):    
+    def __init__(self, starting_url):    
+        self.starting_url = starting_url                       
+        self.visited = set()    
+
+    def start(self):    
+        pass                 
+
+if __name__ == "__main__":    
+    crawler = PyCrawler()    
+    crawler.start()
