57fafecaa4ca77e92292dad1d8293fbd105085a6
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-11-30 07:52:36 +0000

    Create module.joinsplit.js

---
+++ modules/module.joinsplit.js
@@ -0,0 +1,23 @@
+/* ::>> create a class that can be used in the Ui,
+        & in other scripts to join, split and splice
+          Urls, Search terms & json data 
+*/
+export default class{
+  this.input   = input;
+  this.options = [];
+}
+function splice(){
+
+}
+function split(){
+
+}
+function join(){
+
+
+}
+
+let searchTerm = input;
+.splice();
+.split();
+.join();
