23420561230b68d15a9d47efb36dc2f96c9eda3a
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-11-30 08:09:44 +0000

    Update module.joinsplit.js

--- modules/module.joinsplit.js
+++ modules/module.joinsplit.js
@@ -1,20 +1,32 @@
 /* ::>> create a class that can be used in the Ui,
         & in other scripts to join, split and splice
-          Urls, Search terms & json data 
+          Urls, Search terms & json data
+          need to learn more <= still 
 */
-export default class{
+export default class joinSplit{
   this.input   = input;
+  this.inputs  = [];
   this.options = [];
 }
-function splice(){
-
+// ::>>............
+function splice(?){
+spOutput =;
+return output;
 }
-function split(){
+// ::>> ............................
+function split(input, delimeter){
+     if(input == url){
+      //  https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url ::>> See if theres a shorter version...
+      let expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+      let regex = new RegExp(expression);     
+     }
 
+return output;
 }
-function join(){
+// ::>> .................
+function join(inputs, x){
 
-
+return;
 }
 
 let searchTerm = input;
