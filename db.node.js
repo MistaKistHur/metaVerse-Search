009d91eea7fd1d17bfbaeea2f6dceaa200834e63
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-11-30 07:18:25 +0000

    Update db.node.js

--- db.node.js
+++ db.node.js
@@ -1,1 +1,8 @@
-
+const jsonServer  = require('json-server');
+const server      = jsonServer.create();
+const router      = jsonServer.router('x');        // Ania Kubow is passing in here db file hear but i do something different read the damn boooks.
+const middlewares = jsonServer.defaults();
+const port        = process.env.PORT || 3000;
+         server.use(middlewares);
+         server.use(router);
+         server.listen(port);
